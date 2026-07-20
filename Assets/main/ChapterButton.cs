96258d5decc0405dc1bb8ebad3bc25ddc1f514d0
asteridc <mishutka060508@gmail.com>
2026-07-21 01:59:13 +0300

    ф

--- Assets/main/ChapterButton.cs
+++ Assets/main/ChapterButton.cs
@@ -1,17 +1,21 @@
-﻿using System.Collections.Generic;
-using TMPro;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class ChapterButton : MonoBehaviour
 {
     [Tooltip("Название сцены для этой главы")]
     public string sceneName;
 
+    [Header("Lock Visuals")]
+    [SerializeField] private Image lockImage;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Sprite lockSprite;
+
     [SerializeField] private float lockedScaleMultiplier = 0.94f;
-    [SerializeField] private float lockedCanvasAlpha = 0.62f;
-    [SerializeField] private string lockGlyph = "🔒";
+    [SerializeField] private float lockedCanvasAlpha = 0.5f;
 
     private Button button;
     private CanvasGroup canvasGroup;
@@ -19,7 +23,6 @@ public class ChapterButton : MonoBehaviour
     private readonly List<Graphic> cachedGraphics = new List<Graphic>();
     private readonly Dictionary<Graphic, Color> baseColors = new Dictionary<Graphic, Color>();
     private Vector3 baseScale = Vector3.one;
-    private TextMeshProUGUI lockLabel;
 
     private void Awake()
     {
@@ -34,7 +37,6 @@ public class ChapterButton : MonoBehaviour
             baseScale = rectTransform.localScale;
 
         CacheGraphics();
-        EnsureLockLabel();
         RefreshState();
     }
 
@@ -81,9 +83,7 @@ public class ChapterButton : MonoBehaviour
             rectTransform.localScale = unlocked ? baseScale : baseScale * lockedScaleMultiplier;
 
         ApplyGraphicTint(unlocked);
-
-        if (lockLabel != null)
-            lockLabel.gameObject.SetActive(!unlocked);
+        RefreshLockImage(unlocked);
     }
 
     private bool IsUnlocked()
@@ -105,7 +105,7 @@ public class ChapterButton : MonoBehaviour
         Graphic[] graphics = GetComponentsInChildren<Graphic>(true);
         foreach (Graphic graphic in graphics)
         {
-            if (graphic == null)
+            if (graphic == null || graphic == lockImage)
                 continue;
 
             cachedGraphics.Add(graphic);
@@ -117,7 +117,7 @@ public class ChapterButton : MonoBehaviour
     {
         foreach (Graphic graphic in cachedGraphics)
         {
-            if (graphic == null || graphic == lockLabel)
+            if (graphic == null)
                 continue;
 
             if (!baseColors.TryGetValue(graphic, out Color baseColor))
@@ -127,56 +127,28 @@ public class ChapterButton : MonoBehaviour
         }
     }
 
+    private void RefreshLockImage(bool unlocked)
+    {
+        if (lockImage == null || text == null)
+            return;
+
+        if (unlocked || lockSprite == null || text == null)
+        {
+            lockImage.gameObject.SetActive(false);
+            text.gameObject.SetActive(false);
+            return;
+        }
+
+        lockImage.sprite = lockSprite;
+        lockImage.color = Color.white;
+        lockImage.gameObject.SetActive(true);
+        lockImage.transform.SetAsLastSibling();
+    }
+
     private Color ToLockedColor(Color source)
     {
         float grayscale = source.r * 0.299f + source.g * 0.587f + source.b * 0.114f;
         return new Color(grayscale, grayscale, grayscale, source.a);
-    }
-
-    private void EnsureLockLabel()
-    {
-        if (lockLabel != null)
-            return;
-
-        GameObject lockObject = new GameObject("LockOverlay", typeof(RectTransform), typeof(CanvasRenderer), typeof(TextMeshProUGUI));
-        lockObject.transform.SetParent(transform, false);
-        lockObject.transform.SetAsLastSibling();
-
-        RectTransform lockRect = lockObject.GetComponent<RectTransform>();
-        lockRect.anchorMin = new Vector2(0.5f, 0.5f);
-        lockRect.anchorMax = new Vector2(0.5f, 0.5f);
-        lockRect.pivot = new Vector2(0.5f, 0.5f);
-        lockRect.anchoredPosition = new Vector2(0f, 0f);
-        lockRect.sizeDelta = new Vector2(140f, 140f);
-
-        lockLabel = lockObject.GetComponent<TextMeshProUGUI>();
-        lockLabel.text = lockGlyph;
-        lockLabel.alignment = TextAlignmentOptions.Center;
-        lockLabel.fontSize = 72f;
-        lockLabel.color = Color.white;
-        lockLabel.enableWordWrapping = false;
-        lockLabel.overflowMode = TextOverflowModes.Overflow;
-        lockLabel.raycastTarget = false;
-        lockLabel.gameObject.SetActive(false);
-
-        TextMeshProUGUI referenceText = GetReferenceText();
-        if (referenceText != null)
-        {
-            lockLabel.font = referenceText.font;
-            lockLabel.fontSharedMaterial = referenceText.fontSharedMaterial;
-        }
-    }
-
-    private TextMeshProUGUI GetReferenceText()
-    {
-        TextMeshProUGUI[] texts = GetComponentsInChildren<TextMeshProUGUI>(true);
-        foreach (TextMeshProUGUI text in texts)
-        {
-            if (text != null && text != lockLabel)
-                return text;
-        }
-
-        return null;
     }
 
     private bool TryGetChapterNumber(string value, out int chapterNumber)
